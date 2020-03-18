a2fa27986ac4b206013182087a5a2c9b978cf928
Neha Rani <neharani.labh@gmail.com>
2020-03-18 20:35:51 +0800

    changes for dbscripts

--- src/main/resources/db_scripts/dbscripts.sql
+++ src/main/resources/db_scripts/dbscripts.sql
@@ -1,16 +1,16 @@
-use database course_db;
+use course_db;
 
 CREATE TABLE `author` (
   `author_id` varchar(45) NOT NULL,
   `author_name` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`author_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ;
 
 CREATE TABLE `category` (
   `category_id` varchar(45) NOT NULL,
   `category_name` varchar(45) NOT NULL,
   PRIMARY KEY (`category_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+);
 
 
 CREATE TABLE `course` (
@@ -24,7 +24,7 @@ CREATE TABLE `course` (
   KEY `CategoryFK_idx` (`category_id`,`author_id`),
   CONSTRAINT `authorfk` FOREIGN KEY (`author_id`) REFERENCES `author` (`author_id`),
   CONSTRAINT `categoryfk` FOREIGN KEY (`category_id`) REFERENCES `category` (`category_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ;
 
 
 
